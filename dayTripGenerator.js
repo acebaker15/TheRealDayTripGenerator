38606057938b377bc78fe527479d5f5952edbb19
ACE BAKER <acebaker15@gmail.com>
2022-02-28 16:08:15 -0800

    Had to start fresh. This is my actual project.

---
+++ dayTripGenerator.js
@@ -0,0 +1,88 @@
+"use strict"
+
+console.log("Hello World!")
+
+let primoDestinatons = ["Seattle", "Portland", "Paris", "Luckenbach, TX", "San Diego", "Bozeman"];
+
+function randomGenerator(myArray){
+    let randomChoice = myArray[Math.floor(Math.random() * myArray.length)]
+    return randomChoice
+}
+
+function destinationGenerator(){
+    let userIsHappy = false
+
+    while(userIsHappy === false){
+        let randomDestination = randomGenerator(primoDestinatons)
+
+        let userResponse = prompt(`Your randomly generated destination is ${randomDestination}. Are you OK with that?`)
+
+        if(userResponse.toLowerCase() === "yes"){
+            alert(`Enjoy ${randomDestination}`)
+
+            userIsHappy = true
+        }else if(userResponse.toLowerCase() === "no"){
+            userIsHappy = false
+        }
+        
+    }
+    
+}
+
+destinationGenerator()
+
+let primoEats = ["BBQ", "Fish and Chips", "Rocky Mountain Oysters", "Wheat Grass", "VooDoo Doughnuts", "Fondue"];
+
+function randomGenerator(myArray){
+    let randomChoice = myArray[Math.floor(Math.random() * myArray.length)]
+    return randomChoice
+}
+
+function foodGenerator(){
+    let userIsHappy = false
+
+    while (userIsHappy === false){
+        let randomDinner = randomGenerator(primoEats)
+
+        let userResponse = prompt(`Your randomly generated food is ${randomDinner}. Are you OK with that?`)
+
+        if(userResponse.toLowerCase() ==="yes"){
+            alert(`Enjoy your ${randomDinner}`)
+            userIsHappy = true
+        }else if(userResponse.toLowerCase() === "no"){
+            userIsHappy = false
+        }
+
+
+    }
+}
+
+foodGenerator()
+
+let primoTransport = ["Bike", "Foot Mobile", "Uber", "Segway", "Horseback", "Pony Express"];
+
+function randomGenerator(myArray){
+    let randomChoice = myArray[Math.floor(Math.random() * myArray.length)]
+    return randomChoice
+}
+
+function transportGenerator(){
+    let userIsHappy = false
+
+    while(userIsHappy === false){
+        let randomTransport = randomGenerator(primoTransport)
+
+        let userResponse = prompt(`Your randomly generated transportation is ${randomTransport}. Are you OK with that?`)
+
+        if(userResponse.toLowerCase() === "yes"){
+            alert(`Enjoy your ${randomTransport}`)
+            userIsHappy = true    
+        }else if(userResponse.toLowerCase() === "no"){
+        userIsHappy = false
+        }
+
+    }
+}
+
+transportGenerator()
+
